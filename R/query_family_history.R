404140aa4a75a441d77f052d0422d7a2dc934b2f
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2021-10-27 21:18:11 +0100

    Update family history conditions

--- R/query_family_history.R
+++ R/query_family_history.R
@@ -1,7 +1,7 @@
 #' Identify reported family history of a condition
 #'
 #' @description  Internal function. Note conditions fall in two groups
-#' e.g. Y13D = 1 and Y13E = 2. See \href{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=113241}{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=113241}
+#' e.g. Y19 (group 1) and Y20 (group 2). See \href{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=113241}{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=113241}
 #' for more details.
 #'
 #' @param conditions Condition codes for participant.
@@ -19,9 +19,9 @@ query_family_history <- function (conditions, query) {
   }
   # Unknown/prefer not to answer codes for group containing query
   unknown_codes <- if (query %in% c(1, 2, 6, 8, 9, 10)) {
-    c(-11, -13)
-  } else if (query %in% c(3, 4, 11, 12, 13)) {
-    c(-21, -23)
+    c(-11, -13)  # unknown/decline (group 1)
+  } else if (query %in% c(3, 4, 5, 11, 12, 13)) {
+    c(-21, -23)  # unknown/decline (group 2)
   } else {
     stop("Invalid condition code.")
   }
