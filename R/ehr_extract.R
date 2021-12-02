34210a81b2f6bed598ade118a98cf46a50c937bc
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2021-12-02 18:08:51 +0000

    Generalise ehr_extract() to handle record extraction

--- R/ehr_extract.R
+++ R/ehr_extract.R
@@ -1,27 +1,45 @@
-#' Extract observations/test results from raw EHR data
+#' Extract EHR records and values
 #'
-#' @description Extracts values from linked EHR data. Data is extracted from
-#'   the \code{value1} field with the exception of data provider 2 where values
-#'   are extracted from \code{value2} if \code{value1} is empty. Units are taken
-#'   from \code{value3} for data provider 2 (otherwise units are unavailable).
-#'   \code{NA}, zero and duplicate values are dropped.
+#' @description Extracts records/values from linked EHR data. If \code{values}
+#'   is TRUE, values are extracted the \code{value1} field with the exception of
+#'   data provider 2 where values are extracted from \code{value2} if
+#'   \code{value1} is empty. Units are taken from \code{value3} for data
+#'   provider 2 (otherwise units are unavailable). \code{NA}, zero and duplicate
+#'   values are dropped.
 #'
-#' @param ehr_data Data table/frame with UK Biobank clinical event data i.e.
+#' @param ehr_data Data table with UK Biobank clinical event data i.e.
 #'   \code{gp_clinical.txt}.
-#' @param read_codes Values are extracted from these codes. Data table/frame
-#'   with Read v2 (\code{read_2}) and CTV3 (\code{read_3}) columns.
+#' @param read_codes Extract records matching these codes. Data table/frame
+#'   with \code{read_2} (Read v2) and \code{read_3} (CTV3) columns.
+#' @param values Extract values from EHR records. Replaces the \code{value1},
+#'   \code{value2}, \code{value3} columns with a single \code{value} column and
+#'   a \code{unit} column. Default is currently TRUE but will change to FALSE
+#'   in a future release.
 #'
-#' @return Data table with \code{value} and \code{unit} columns.
+#' @return Filtered EHR data with \code{value} and \code{unit} columns if
+#'   \code{values} is TRUE.
 #'
 #' @export
 #'
-ehr_extract <- function (ehr_data, read_codes) {
+ehr_extract <- function (ehr_data, read_codes, values = NULL) {
   read_2 = read_3 = NULL
-  # Check inputs
+  # Check arguments
+  argument_check(ehr_data, "data_table")
   ehr_data <- check_gp_clinical(ehr_data)
   codes <- check_read_codes(read_codes)
+  # Warn on values argument
+  if (is.null(values)) {
+    values <- TRUE
+    message('Argument "values" will default to FALSE in a future release. Use values = TRUE to keep current behaviour.')
+  } else {
+    argument_check(values, "flag")
+  }
   # Extract and clean observations/test results
   raw_values <- ehr_data[read_2 %in% setdiff(read_codes$read_2, "-") |
                            read_3 %in% setdiff(read_codes$read_3, "-")]
-  extract_pc_values(raw_values)
+  if (values) {
+    extract_pc_values(raw_values)
+  } else {
+    raw_values[]
+  }
 }
